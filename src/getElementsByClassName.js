cc81113c50712f745de61819c32f4f5c1fb95fba
Jeff Bentler <bentljm@gmail.com>
2016-11-28 18:12:47 -0800

    Updated getElementsByClassName.js and stringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  //create new empty array to hold elements
+  var elements = [];
+  //search function that inspects nodes
+  function search(parentNode, elements, className){
+  	//find childNodes of given element
+    var nodes = parentNode.childNodes;
+    for (var i = 0; i < nodes.length; i++){
+    	var node = nodes[i];
+    	//find class list of node
+        var classes = node.classList;
+        //tests whether target className is in class list
+        if (classes && classes.contains(className)) {
+            elements.push(node);
+        }
+        if (node.childNodes[0]){
+            search(node, elements, className);
+        }
+    }
+  }
+  //call search function
+  search(document, elements, className);
+
+  return elements;
+
 };
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,65 @@
-// this is what you would do if you liked things to be easy:
+// this is what you would do ifyou liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // obj = null
+  if(obj === null) {
+    return "null";
+  }
+  // obj is unstringifiable - functions and undefined
+  if(typeof obj === 'undefined' || typeof obj === 'function') { 
+  	return; 
+  }
+  // obj is string
+  if(typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+  // obj is array
+  if(Array.isArray(obj)) {
+  	//tests whether obj is not empty
+    if(obj.length) {
+      var stringified = [];
+      for(var i = 0; i < obj.length; i++) {
+      	var element = obj[i]
+      	//go thru and stringify each element of array and add result to stringified
+        stringified.push(stringifyJSON(element));
+      }
+      //join array and add brackets
+      return '[' + stringified.join(",") + ']';
+    } else {
+      return '[]';
+    }
+  }
+  // obj is object
+  if(typeof obj === "object") {
+  	//get all properties of obj
+    var keys = Object.keys(obj);
+    if(keys.length) {
+      var stringified = '';
+      //loop thru array of properties of object
+      for(var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        //tests whether key and obj[key] are stringifiable
+        if(typeof key !== 'function' && typeof obj[key] !== 'function' && typeof obj[key] !== "undefined") {
+          //stringifies key and obj[key] with : in between and/or , at the end
+          if(i === keys.length - 1) {
+            stringified += stringifyJSON(key) + ':' + stringifyJSON(obj[key]); 
+          } else {
+            stringified += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ','; 
+          }
+        }
+      }
+      //return string with curly brackets
+      return '{' + stringified + '}';
+    } else {
+      return '{}';
+    }
+  }
+
+  // Everything else
+  return obj.toString();
+
 };
+
